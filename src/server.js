f639cc012134c6fdbab4ff7e7c3b487f1a1c4596
projects-FORCHRISTALONE <orimadegunemma@gmail.com>
2026-01-21 17:37:36 +0100

    updated server.js by removing frontend impacting code

--- src/server.js
+++ src/server.js
@@ -17,13 +17,7 @@ const PORT = process.env.PORT || 3000
 app.use("/api/auth",authRoutes );
 app.use("/api/messages",messageRoutes );
 
-if(process.env.NODE_ENV=="production"){
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("*", (req,res)=>{
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
-    })
-}
 
 console.log(__dirname)
 app.listen(3000, ()=>{

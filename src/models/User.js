f579532efd8d4e201a8fddec8dfa4f9a10107a83
projects-FORCHRISTALONE <orimadegunemma@gmail.com>
2026-01-22 00:45:49 +0100

    initial signup auth added by God's grace alone

---
+++ src/models/User.js
@@ -0,0 +1,32 @@
+// BY GOD'S GRACE ALONE
+
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    fullName:{
+        type: String,
+        required: true
+    },
+    password:{
+        type: String,
+        required: true,
+        minlength: 6,
+    },
+    profilePic:{
+        type: String,
+        default: "",
+    },
+},
+{
+    timestamps: true
+}
+)
+
+const User =  mongoose.model("User", userSchema);
+
+export default User
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,12 +1,11 @@
 // BY GOD'S GRACE ALONE
 
 import express from "express"
+import { signup } from "../controllers/auth.controller.js"
 
 const router = express.Router()
 
-router.get("/signup", (req,res)=> {
-    res.send("Signup endpoint")
-})
+router.post("/signup", signup )
 
 router.get("/logout", (req,res)=> {
     res.send("Logout endpoint")

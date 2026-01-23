baae1aae4dd7daa4000a96f8310542d7e5d082a4
projects-FORCHRISTALONE <orimadegunemma@gmail.com>
2026-01-23 02:36:17 +0100

    update profile endpoint added

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,31 +3,32 @@ import User from "../models/User.js";
 
 import bcrypt from "bcryptjs"
 
-import {generateToken }from "../lib/utils.js"
+import { generateToken } from "../lib/utils.js"
 import { sendWelcomeEmail } from "../../emails/emailHandlers.js";
 
 import { ENV } from "../lib/env.js";
+import cloudinary from "../lib/cloudinary.js";
 
 
-export const signup = async (req,res)=> {
-    try{
-        const {fullName, email, password} = req.body;
+export const signup = async (req, res) => {
+    try {
+        const { fullName, email, password } = req.body;
 
-        if (!fullName || !email || !password){
-            return res.status(400).send({message: "All fields are required"})
+        if (!fullName || !email || !password) {
+            return res.status(400).send({ message: "All fields are required" })
         }
 
-        if (password.length < 6){
-            return res.status(400).send({message: "Password must be at least 6 characters"})
+        if (password.length < 6) {
+            return res.status(400).send({ message: "Password must be at least 6 characters" })
         }
 
-        const  emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-        if (!emailRegex.test(email)){
-            return res.status(400).send({message: "Invalid email format"})
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+        if (!emailRegex.test(email)) {
+            return res.status(400).send({ message: "Invalid email format" })
         }
 
-        const user = await User.findOne({email})
-        if(user) return res.status(400).send({message: "Email already exists"})
+        const user = await User.findOne({ email })
+        if (user) return res.status(400).send({ message: "Email already exists" })
 
         const salt = await bcrypt.genSalt(10)
         const hashedPassword = await bcrypt.hash(password, salt)
@@ -38,9 +39,9 @@ export const signup = async (req,res)=> {
             password: hashedPassword
         })
 
-        if (newUser){
+        if (newUser) {
             await newUser.save()
-            generateToken(newUser._id,res)
+            generateToken(newUser._id, res)
 
             res.status(201).json({
                 _id: newUser._id,
@@ -49,56 +50,80 @@ export const signup = async (req,res)=> {
                 profilePic: newUser.profilePic
             })
 
-            try{
+            try {
                 await sendWelcomeEmail(newUser.email, newUser.fullName, ENV.CLIENT_URL)
 
-            } catch(err){
+            } catch (err) {
                 console.error("Failed to fetch welcome email", err)
             }
-        }else {
-            res.status(400).json({message: "Invalid user data"})
+        } else {
+            res.status(400).json({ message: "Invalid user data" })
         }
 
-    }catch(err){
-       if (err?.code === 11000) {
-           return res.status(409).send({ message: "Email already exists" })
-       }
-       console.error(err)
-       return res.status(500).send({ message: "Internal server error" })
-   }
-    
-}
-
-
-export const login = async (req,res) => {
-    try{
-        const {email, password} = req.body;
-        const user = await User.findOne({email})
-        if(!user) return res.status(400).send({message: 'Invalid credentials'})
-
-        const isPasswordCorrect = await bcrypt.compare(password, user.password)
-        if(!isPasswordCorrect) return res.status(400).send({message: "Invalid credentials"})
-        
-        generateToken(user._id, res)
-
-
-        res.status(200).send({
-            _id : user._id,
-            fullName : user.fullName,
-            email : user.email,
-            profilePic : user.profilePic
-        });
-
-    } catch(err){
-        console.error("Error in login controller:", error)
-        res.status(500).send({message: "Internal server error"})
+    } catch (err) {
+        if (err?.code === 11000) {
+            return res.status(409).send({ message: "Email already exists" })
+        }
+        console.error(err)
+        return res.status(500).send({ message: "Internal server error" })
     }
 
 }
 
-export const logout = (_,res) => {
-    res.cookie("jwt", "", {maxAge: 0})
-    res.status(200).send({message: "Logged out successfully"})
+
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!user || !password) return res.status(400).send({ message: "Email and password required" })
+        const user = await User.findOne({ email })
+        if (!user) return res.status(400).send({ message: 'Invalid credentials' })
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password)
+        if (!isPasswordCorrect) return res.status(400).send({ message: "Invalid credentials" })
+
+        generateToken(user._id, res)
+
+
+        res.status(200).send({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic
+        });
+
+    } catch (err) {
+        console.error("Error in login controller:", error)
+        res.status(500).send({ message: "Internal server error" })
+    }
+
 }
 
+export const logout = (_, res) => {
+    res.cookie("jwt", "", { maxAge: 0 })
+    res.status(200).send({ message: "Logged out successfully" })
+}
+
+export const updateProfile = async (res, req) => {
+    try {
+        const { profilePic } = req.body
+
+        if (!profilePic) return res.status(400).send({ message: "Profile pic is required" })
+
+        const userId = req.user._id
+
+        const uploadResponse = await cloudinary.uploader.upload(profilePic)
+
+        const updatedUser = await User.findByIdAndUpdate(
+            userId, 
+            { profilePic: uploadResponse.secure_url }, 
+            { new: true }
+        )
+
+        res.status(200).json(updatedUser)
+    } catch (err) {
+        console.log("Error in update profile:", err);
+        res.status(500).json({message: "Internal server error"})
+    }
+}
 
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,7 +1,8 @@
 // BY GOD'S GRACE ALONE
 
 import express from "express"
-import { signup, login, logout } from "../controllers/auth.controller.js"
+import { signup, login, logout, updateProfile } from "../controllers/auth.controller.js"
+import { protectRoute } from "../middleware/auth.middleware.js"
 
 const router = express.Router()
 
@@ -10,5 +11,11 @@ router.post("/signup", signup )
 router.post("/logout", logout)
 
 router.post("/login", login)
+
+router.put("/update-profile", protectRoute, updateProfile)
+
+router.get("/check", protectRoute, (req,res)=>{
+    res.status(200).json(req.user)
+})
 
 export default router
